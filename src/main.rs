3be467df862c2e127e10780061be902894fa1ec1
Lukáš Hozda <luk.hozda@gmail.com>
2017-10-06 21:30:17 +0200

    implement handing in & sending emails

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate lettre;
 extern crate reqwest;
 extern crate serde_json;
 #[macro_use]
@@ -5,7 +6,11 @@ extern crate serde_derive;
 
 use reqwest::Client;
 use reqwest::header::{Accept, qitem};
+use lettre::email::EmailBuilder;
+use lettre::transport::EmailTransport;
+use lettre::transport::smtp::SmtpTransportBuilder;
 
+use std::time::Instant;
 use std::process::exit;
 
 #[derive(Deserialize, Clone, Debug)]
@@ -19,12 +24,14 @@ struct Team {
 struct Repository {
     name: String,
     description: Option<String>,
+    html_url: String,
+    has_issues: bool,
     id: i32,
 }
 
 const PREFIX: &'static str = "s_";
 const ORG_NAME: &'static str = "rust-gjk";
-const TEAM_NAME: &'static str = "rok-2017-2018"; //actually a slug
+const TEAM_NAME: &'static str = "rok-2017-2018"; //actually slug of the team
 const AUTH_TOKEN: &'static str = "lol nope";
 
 fn main() {
@@ -86,6 +93,7 @@ fn main() {
         }
     };
 
+    // move repositories to team
     for repo in repos
             .iter()
             .filter(|x| x.name.starts_with(PREFIX) && !moved_repos.contains(x)) {
@@ -104,5 +112,65 @@ fn main() {
         if let Err(e) = res {
             println!("err: {}", e);
         }
+    }
+
+    // check for handed in repositories - 'r' topic
+    // 'n' topic makes the repo ignored and topics et al. unchanged
+    // 'o' means handed in and recognized by the bot
+    for repo in moved_repos {
+    	let topics = match Client::new()
+    		.get(&format!("https://api.github.com/repos/{}/{}/topics",
+    			          ORG_NAME,
+    			          &repo.name
+    			))
+    		.basic_auth(AUTH_TOKEN, Some("x-oauth-basic"))
+    		.header(Accept(vec![qitem("application/vnd.github.v3+json".parse().expect("potato"))]))
+    		.send()
+    		.expect("why no topic")
+    		.json::<Vec<String>>() {
+    		Ok(t) => t,
+    		Err(e) => {
+    			println!("err: {}", e);
+    			exit(-1)
+    		}
+    	};
+
+    	if topics.contains(&"r".to_string())
+    	&& !topics.contains(&"n".to_string()) {
+    		let res = Client::new()
+	            .put(&format!("https://api.github.com/repos/{}/{}/topics",
+	                          ORG_NAME,
+	                          &repo.name))
+	            .basic_auth(AUTH_TOKEN, Some("x-oauth-basic"))
+	            .header(Accept(vec![qitem("application/vnd.github.v3+json".parse().expect("potato"))]))
+	            .body("{ [\"o\"] }")
+	            .send();
+
+	        if let Err(e) = res {
+	            println!("err: {}", e);
+	        }
+
+	        let email = EmailBuilder::new()
+	            .to(("luk.hozda@gmail.com", "Lukáš Hozda"))
+	            .from("rust@magnusi.tech")
+	            .subject(&format!("Úkol odevzdán: {}", &repo.name))
+	            .text(&format!("Repozitář byl právě označen jako odevzdaný.
+
+	            	   URL: {}
+	            	   Název: {}
+	            	   Čas: {:?}
+
+	            	   ______
+	            	   gjkbot
+	            ", &repo.html_url, &repo.name, Instant::now()))
+	            .build().unwrap();
+
+	        let mut mailer = SmtpTransportBuilder::localhost().unwrap().build();
+			if let Err(e) = mailer.send(email) {
+				println!("err: {}", e);
+			}
+    	}
+
+
     }
 }
